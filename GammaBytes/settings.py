a156369af99074fbe3a788ad080272c533473a3d
Devansh Agrahari <agradevu22@gmail.com>
2024-03-16 02:07:10 +0530

    Initial Commit

--- GammaBytes/settings.py
+++ GammaBytes/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DJANGO_DEBUG', 1)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
